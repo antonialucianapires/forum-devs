86f29fb3d57e107b990a7b715717b2859e09439f
Antonia Luciana Pires <46853268+antonialucianapires@users.noreply.github.com>
2020-02-13 16:55:58 -0300

    Update database.js

--- database/database.js
+++ database/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const connection = new Sequelize('devforum', 'root', 'bola123', {
+const connection = new Sequelize('devforum', 'root', '******', {
     host: 'localhost',
     dialect: 'mysql'
 });
